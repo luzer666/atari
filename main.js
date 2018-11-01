5ab0c581dbb0b2800fc535874793dab329ec6a45
irenicus <30894390+luzer666@users.noreply.github.com>
2018-11-01 10:24:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-// Load the http module to create an http server.
-const http = require('http');
+var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
+http.createServer(function(request, response){
+  response.writeHead(200, {'Content-type':'text/plan'});
+  response.write('Hello Node JS Server Response');
+  response.end( );
 
-// Last, but not least, listen on port 8080
-// The environment variable PORT is automatically defined and equals to 8080
-server.listen(process.env.PORT, '0.0.0.0');
+}).listen(8080);
 
 
 var Discordie = require("discordie");
