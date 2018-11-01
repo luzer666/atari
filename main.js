d8bfcf900c901e1d05bef8a2c677b09317911901
irenicus <30894390+luzer666@users.noreply.github.com>
2018-11-01 20:49:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ http.createServer(function(request, response){
   response.write('Hello Node JS Server Response');
   response.end( );
 
-}).listen(8080);
+server.listen(8080);
 
 
 var Discordie = require("discordie");
