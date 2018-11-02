df8f809f0645a9a10c878d45518e47d51704e136
irenicus <30894390+luzer666@users.noreply.github.com>
2018-11-02 14:22:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,8 +11,10 @@ http.createServer(function(request, response){
 var Discordie = require("discordie");
 var client = new Discordie({autoReconnect: true});
 
-var token = process.env.TOKEN
-client.connect({token});
+client.connect({
+    token: "MzMyNTMwODUyNDM5MzkyMjY4.Dr3gyw.9IJF4cEEAi3KIat97XfogL6R1JU"
+});
+
 client.Dispatcher.on("GATEWAY_READY", e => {
     console.log("Username: " + client.User.username);
 });
