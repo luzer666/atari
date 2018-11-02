76671fe99cf7374e069790c2b3aacae137b3b6b9
irenicus <30894390+luzer666@users.noreply.github.com>
2018-11-02 09:58:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ http.createServer(function(request, response){
 
 var Discordie = require("discordie");
 var client = new Discordie({autoReconnect: true});
-const token = process.env.TOKEN;
+const token = process.env["TOKEN"]
 
 
 client.connect(token);
